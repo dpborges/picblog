c9ac06eff8bcbc69a01e68ae7e6af411ee41943b
dpborges <dpborges@gmail.com>
2021-06-23 21:04:17 -0400

    created sitemap libary and function to match props in an object

--- utils/sitemap.mjs
+++ utils/sitemap.mjs
@@ -1,57 +1,148 @@
 import fs from 'fs-extra';
 import path from 'path';
-import readFromFile from './filesystem.mjs';
+// import readFromFile from './filesystem.mjs';
 // import toPairs from 'lodash';
 import intersectionWith from 'lodash';
 // import * as R from 'ramda';
 // const { includes } = R;
 // import includes from 'ramda/src/includes'
-import R from 'ramda';
+import { filterObjArrayBy } from './object.mjs';
 
+// **********************************************************************
+// Define main function to generate sitemap, and call it immediatedly after.
+// **********************************************************************
 
+/* Set the source data file */
 const fileName = 'contentmetadata.json';
 const filePath = path.join(process.cwd(), '../', 'data', fileName )  /* cwd returns project folder */
+const outputFile = path.join(process.cwd(), '../', 'public', 'generatedSitemap.xml' )  /* cwd returns project folder */
 
-/* Read content meta data */
-// async function getJsonMetaData(filePath)  {
-//   readFromFile(filePath).then((metadata) => {
-//     // console.log(JSON.stringify(metadata,null,2));
-//     return metadata;
-//   }).catch(function(e) {
-//     console.log(e)
-//   });
-// }
-
-const filterObj = { id: "0015", wordCount: 915}
-const filterInputBy = (inputArray, filterObj) => {
-  let filteredData = inputArray.filter((contentmd) => {    
-    console.log("looping thru contentmd ", contentmd)
-    return contentmd.id === "0022"
-  })
-  console.log(`filteredata is : ${JSON.stringify(filteredData,null,2)}`);
-  return filteredData
-}
-
+/* Define main function to generate sitemap from data file provided  */
 async function genSiteMap(filePath) {
-  /* getJsonMetadata
-     convertToJsObject
-     filterBlogPosts
-     writeUrlsetTag
-     writeCommentBLock("the comment")
-     writeUrlTag(object with metadata for single blogpost)
-  */
-  let jsonMetadata   = await readFromFile(filePath);
-  let jsMetadataObjs = JSON.parse(jsonMetadata);
-  // console.log(JSON.stringify(jsMetadataObjs,null,2));
-  // let filteredObjs   = filterInputBy(jsMetadataObjs, filterObj);
-  let result = R.includes({ wordCount: 915}, jsMetadataObjs)
-  console.log(`is included in metadataObjs ${result}`)
+  let mdObjectArray      = await fs.readJson(filePath); /* read JSON md file and convert to object array */
+  let blogPostsArray     = filterObjArrayBy(mdObjectArray, { type: "post" });
+  let landingPageArray   = filterObjArrayBy(mdObjectArray, { type: "landingPage" });
+  let sitemap  = new SiteMap("picblog-sitemap");     /* instantiate a sitemap */
+  sitemap.addUrlsetOpenTag();  /* add <urlset ...> tag */
+  addLandingPageToSiteMap(landingPageArray, sitemap);
+  addBlogPostsToSiteMap(blogPostsArray, sitemap);
+  sitemap.addUrlsetCloseTag(); /* close </urlset>  tag */
+  // sitemap.print();
+  sitemap.writeTo(outputFile);
+  console.log("Sitemap generated")
 }
 
-// R.includes DID NOT WORK. CREATE MY OWN FUNCTION CALLED matchProperties(obj1, obj2), THAT RETURNS
-// TRUE IF PROPERTIES IN OBJ1 ARE IN OBJ2 AND VALUES ARE EQUAL
-
+/* Call the generate site map function  */
 genSiteMap(filePath);
-// console.log(Object.keys(filterObj))
-// let inter = intersectionWith([{ id: "0015"}], )
-// console.log(`intersection ${inter}`)
+
+// **********************************************************************
+// SiteMap Class allows you to instantiate a sitemap, add the relevant xml
+// tags and write it to file 
+// **********************************************************************
+class SiteMap {
+
+  constructor(name) { 
+    this.name = name; 
+    this.sitemapData = [];
+  }
+
+  _append(data) {
+    this.sitemapData.push(data);
+  }
+
+  addUrlsetOpenTag() {
+    let xmlDeclTag = "<?xml version='1.0' encoding='UTF-8'?>" + "\n";
+    let urlsetOpenTag = `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">`;
+    this._append(xmlDeclTag);
+    this._append(urlsetOpenTag + "\n");
+    return this;
+  }
+  addUrlsetCloseTag() {
+    this._append("</urlset>");
+  }
+
+  addUrlOpenTag() {
+    this._append("<url>" + "\n");
+    return this;
+  }
+  addUrlCloseTag() {
+    this._append("</url>" + "\n");
+    return this;
+  }
+  
+  addLocTag(url) {
+    const locTag = `  <loc>${url}</loc>`;
+    this._append(locTag + "\n");
+    return this;
+  }
+
+  addImageTag(image) {
+    const imageTag = `  <image:image>
+    <image:loc>${image.loc}</image:loc>
+    <image:title>${image.title}</image:title>
+  </image:image>`;
+    this._append(imageTag + "\n");
+    return this;
+  }
+
+  addCommentBLock(comment) {
+    const commentBlock = `<!--  ****************************************************************************  -->
+<!--  ${comment}  -->
+<!--  ****************************************************************************  -->`
+    this._append(commentBlock + "\n");
+    return this;
+  }
+  
+  print() {
+    this.sitemapData.map((textblock) => {
+      console.log(textblock)
+    })
+  }
+
+  writeTo(file) {
+    let data = "";
+    this.sitemapData.map((textblock) => {
+      data = data + textblock
+    })
+    fs.writeFileSync(file, data);
+  }
+} /* end of SiteMap class */
+
+
+// **********************************************************************
+// PIC Helper function specifically to add the individual the landingpage,
+// resource pages, and individual blog posts to the sitemap
+// **********************************************************************
+const addBlogPostsToSiteMap = (postsObjArray, sitemap) => {
+
+  let blogUrl   = "https://pitchinclub.com/blog";
+  let baseUrl  = "https://pitchinclub.com";
+
+  sitemap.addCommentBLock("INDIVIDUAL BLOG POSTS");
+  /* add array of blogposts to sitemap */
+  postsObjArray.map( (post) => {
+    sitemap.addUrlOpenTag();
+      sitemap.addLocTag(`${blogUrl}/${post.slug}`);
+      sitemap.addImageTag({loc: `${baseUrl}${post.images[0].src}`, title: `${post.images[0].alt}`});
+    sitemap.addUrlCloseTag();
+  }) 
+}
+
+ /* Add landing page to sitemap */
+const addLandingPageToSiteMap = (postsObjArray, sitemap) => {
+
+  let baseUrl  = "https://pitchinclub.com";
+  
+  sitemap.addCommentBLock("LANDING PAGE AND LANDING PAGE IMAGES");
+  postsObjArray.map( (post) => {
+    sitemap.addUrlOpenTag();
+      sitemap.addLocTag(`${baseUrl}`);
+      sitemap.addImageTag({loc: `${baseUrl}${post.images[0].src}`, title: `${post.images[0].alt}`});
+      sitemap.addImageTag({loc: `${baseUrl}${post.images[1].src}`, title: `${post.images[1].alt}`});
+      sitemap.addImageTag({loc: `${baseUrl}${post.images[2].src}`, title: `${post.images[2].alt}`});
+      sitemap.addImageTag({loc: `${baseUrl}${post.images[3].src}`, title: `${post.images[3].alt}`});
+      sitemap.addImageTag({loc: `${baseUrl}${post.images[4].src}`, title: `${post.images[4].alt}`});
+    sitemap.addUrlCloseTag();
+  }) 
+}
+
